6753b7fc7f55e0795e25431fb9c1e711ed45e90c
Tobetch <takutobe@gmail.com>
2021-07-12 12:59:19 +0900

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Basic from "./Basic";
+import BasicBoilerplate from "./BasicBoilerplate";
+
+function App() {
+  return (
+    <div className="App">
+      <Basic></Basic>
+      <BasicBoilerplate></BasicBoilerplate>
+    </div>
+  );
+}
+
+export default App;
